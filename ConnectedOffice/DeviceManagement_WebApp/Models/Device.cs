78a26277010034f597d103a36437c3193bcd52c9
Dennis van Tonder <dennisvantonder@outlook.com>
2022-09-21 08:41:09 +0200

    Added connection string to project

--- ConnectedOffice/DeviceManagement_WebApp/Models/Device.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Models/Device.cs
@@ -20,7 +20,7 @@ namespace DeviceManagement_WebApp.Models
         public Guid ZoneId { get; set; }
         [DisplayName("Status")]
         public string Status { get; set; }
-        [DisplayName("Is Active")]
+        [DisplayName("IsActive")]
         public bool IsActive { get; set; }
         [DisplayName("Date Created")]
         public DateTime DateCreated { get; set; }
