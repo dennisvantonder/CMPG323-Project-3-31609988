b8eeeb24004348193b7ccac405b225ef98043dfe
Dennis van Tonder <dennisvantonder@outlook.com>
2022-09-21 09:05:20 +0200

    Added Repository classes and interfaces for each model.

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/IZoneRepository.cs
@@ -0,0 +1,9 @@
+﻿using DeviceManagement_WebApp.Models;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public interface IZoneRepository : IGenericRepository<Zone>
+    {
+        Zone GetMostRecentZone();
+    }
+}
---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/ZoneRepository.cs
@@ -0,0 +1,18 @@
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+using System.Linq;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class ZoneRepository : GenericRepository<Zone>, IZoneRepository
+    {
+        public ZoneRepository(ConnectedOfficeContext context) : base(context)
+        {
+        }
+
+        public Zone GetMostRecentZone()
+        {
+            return _context.Zone.OrderByDescending(zone => zone.DateCreated).FirstOrDefault();
+        }
+    }
+}
